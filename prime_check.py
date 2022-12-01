bcc6eae6f0227aa53c8f02d7ccff61e0c4770160
Pradhiman <90816503+Pradhiman@users.noreply.github.com>
2022-12-01 21:50:19 +0530

    Add files via upload
    This is my first version of this code. There might be a bit of redundancy, but hope y'all like it :)

---
+++ prime_check.py
@@ -0,0 +1,60 @@
+from math import*
+a = 1989
+
+
+def is_prime(a):
+
+    for i in range(2, floor(sqrt(abs(a))) + 1):
+        if a % i == 0:
+            return ["number is not prime", 0]
+            break
+    return ["number is prime", 1]
+
+
+def factors(a):
+    b = abs(a)
+    factor = [b]
+    for i in range(2, int(b / 2) + 1):
+        if a % i == 0:
+            factor.append(i)
+    factor.sort()
+    factor_str = map(str, factor)
+    factors = ', '.join(factor_str)
+    return [factor, factors]
+
+
+def factorisation(a):
+    p = abs(a)
+    power_list = [0, "", "\u00B2", "\u00B3", "\u2074",
+                  "\u2075", "\u2076", "\u2077", "\u2078", "\u2079"]
+    prime_list = []
+    while p >= 3:
+        for index, subject in enumerate(factors(p)[0]):
+            if is_prime(subject)[1] == 1:
+                p = p / subject
+                prime_list.append(subject)
+
+    if p < 3:
+        if not p == 1:
+            prime_list.insert(0, p)
+        if 1 in prime_list:
+            prime_list.remove(1)
+    set_1 = set(prime_list)
+    list_1 = list(set_1)
+    list_1.sort()
+
+    prime_factor = []
+    for i, s in enumerate(list_1):
+        p = prime_list.count(s)
+        prime_factor.append(str(s) + power_list[p])
+
+    prime_factors = ' x '.join(prime_factor)
+
+    return prime_factors
+
+
+print(is_prime(a)[0])
+if factors(a):
+    print("no. of factors are ", len(factors(a)[0]) + 1)
+    print("factors are ", '1,', factors(a)[1])
+print("prime factorisation: ", factorisation(a))
